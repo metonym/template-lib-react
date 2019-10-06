5bcb42a6962fcc4187a6f7838f76322122a5bd13
Eric Liu <ericyl.us@gmail.com>
2019-10-06 08:13:04 -0700

    chore: convert Component to functional component

--- src/Component.tsx
+++ src/Component.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
 
-class Component extends React.Component<{}, { data: number[] }> {
-  public state = {
-    data: [0, 1, 2]
-  };
+function Component() {
+  const [data] = React.useState([0, 1, 2]);
 
-  public componentDidMount() {
-    this.setState({ data: [] });
-  }
-
-  public render() {
-    return <React.Fragment>{this.state.data}</React.Fragment>;
-  }
+  return (
+    <>
+      {data.map(item => (
+        <div key={item}>{item}</div>
+      ))}
+    </>
+  );
 }
 
 export default Component;
--- tests/Component.spec.tsx
+++ tests/Component.spec.tsx
@@ -1,26 +1,11 @@
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import * as React from 'react';
 import Component from '../src/Component';
 
-let method: jest.SpyInstance<void, []>;
-
 describe('Component', () => {
-  beforeEach(() => {
-    if (method) {
-      method.mockClear();
-    }
-  });
-
-  it('renders without crashing', () => {
-    method = jest.spyOn(Component.prototype, 'componentDidMount');
-    const wrapper = mount<Component>(<Component />);
-    expect(method).toHaveBeenCalledTimes(1);
-    expect(wrapper.state().data).toEqual([]);
-  });
-
   it('matches the snapshot', () => {
-    const wrapper = shallow<Component>(<Component />);
+    const wrapper = shallow(<Component />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
